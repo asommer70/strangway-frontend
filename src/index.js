3104456452943a196401a5081a685f22093191bb
Adam Sommer <adam@thehoick.com>
2017-10-11 23:48:12 -0400

    Initial import.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Strangway... and things from React...</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: '/',
+    filename: './dist/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
