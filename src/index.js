7f634803f72ca7ed1f98443d68ddfe74e10d9a6e
Adam Sommer <adam@thehoick.com>
2017-11-19 07:10:14 -0500

    Working on integrating React Router.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 import MainScss from './scss/main.scss';
 
 import Main from './components/main';
+import Signup from './components/signup';
+import Signout from './components/signout';
+import Login from './components/login';
 
 const client = new ApolloClient({
   link: new HttpLink({uri: 'http://localhost:4000/graphql'}),
@@ -20,9 +27,14 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div id="app" className="container">
-          <Main />
-        </div>
+        <Router>
+          <div id="app" className="container">
+            <Route exact path="/" component={Main}/>
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Signout} />
+          </div>
+        </Router>
       </ApolloProvider>
     )
   }
